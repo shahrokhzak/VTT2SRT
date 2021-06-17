956afec12a774ea1b44fd8de8f93da73835ae4f5
zakpro <shahrokh.zakipoor@gmail.com>
2021-06-17 19:02:55 +0430

    Add project files.

---
+++ consoleAppTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace consoleAppTest
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var path = args.Length == 0 ? AppContext.BaseDirectory : args[0];
+
+            var files = Directory.GetFiles(path);
+            foreach (var variable in files)
+            {
+                if (Path.GetExtension(variable) != ".vtt") continue;
+                Console.WriteLine(variable);
+                VttConverter.ConvertToSrt(variable, path +@"\"+ Path.GetFileNameWithoutExtension(variable) + ".srt");
+            }
+        }
+    }
+}
